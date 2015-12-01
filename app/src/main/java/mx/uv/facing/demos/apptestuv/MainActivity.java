2938631570a01f18e872b06cdf7010f95c142e73
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2015-11-30 23:17:48 -0600

    test

--- app/src/main/java/mx/uv/facing/demos/apptestuv/MainActivity.java
+++ app/src/main/java/mx/uv/facing/demos/apptestuv/MainActivity.java
@@ -1,7 +1,12 @@
 package mx.uv.facing.demos.apptestuv;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.provider.CalendarContract;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
@@ -14,9 +19,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
@@ -68,40 +78,95 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final VideoView videoView = (VideoView) findViewById (R.id.videoView);
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView (videoView);
-        mediaController.setMediaPlayer (videoView);
 
-        String videoPath = "https://dl.dropboxusercontent.com/u/52679306/movie.mp4";
-        Uri videoURI = Uri.parse (videoPath);
-        videoView.setMediaController (mediaController);
-        videoView.setVideoURI(videoURI);
-        videoView.requestFocus();
-
-        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+        Button b = (Button) findViewById (R.id.btnCalendar);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
-                Toast.makeText (getBaseContext(), "Error", Toast.LENGTH_LONG).show ();
-                return true;
+            public void onClick(View view) {
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2015, 12, 01,12, 30);
+
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2015, 12, 01, 13, 45);
+
+                ContentResolver cr = getContentResolver();
+                ContentValues values = new ContentValues();
+                TimeZone timeZone = TimeZone.getDefault();
+                values.put(CalendarContract.Events.DTSTART, beginTime.getTimeInMillis ());
+                values.put(CalendarContract.Events.DTEND, endTime.getTimeInMillis ());
+                values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
+                values.put(CalendarContract.Events.TITLE, "Walk The Dog");
+                values.put(CalendarContract.Events.DESCRIPTION, "My dog is bored, so we're going on a really long walk!");
+                values.put(CalendarContract.Events.CALENDAR_ID, 3);
+
+                try {
+                    Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+                    Log.i ("A", uri.toString ());
+                } catch (SecurityException ex) {
+
+                }
+            }
+
+            int getCalendarId () {
+                Uri uri = CalendarContract.Calendars.CONTENT_URI;
+
+                String [] projection = new String [] {
+                        CalendarContract.Calendars._ID,
+                        CalendarContract.Calendars.ACCOUNT_NAME,
+                        CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+                        CalendarContract.Calendars.NAME,
+                        CalendarContract.Calendars.CALENDAR_COLOR
+                };
+
+                Cursor calendarCursor = managedQuery (uri, projection, null, null, null);
+
+                calendarCursor.moveToFirst();
+                while (calendarCursor.moveToNext()) {
+                    Log.i("A", calendarCursor.getString(calendarCursor.getColumnIndex("ACCOUNT_NAME")));
+                    Log.i ("A", calendarCursor.getString (calendarCursor.getColumnIndex("_ID")));
+                }
+
+                calendarCursor.moveToFirst();
+                return calendarCursor.getColumnIndex("_id");
             }
         });
 
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                videoView.start ();
-            }
-        });
-
-        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                mediaPlayer.pause();
-            }
-        });
-
+//        final VideoView videoView = (VideoView) findViewById (R.id.videoView);
+//        MediaController mediaController = new MediaController (this);
+//        mediaController.setAnchorView (videoView);
+//        mediaController.setMediaPlayer (videoView);
+//
+//        //String videoPath = "https://dl.dropboxusercontent.com/u/52679306/movie.mp4";
+//        String videoPath = "android.resource://mx.uv.facing.demos.apptestuv/" + R.raw.movie;
+//        Uri videoURI = Uri.parse (videoPath);
+//        videoView.setMediaController (mediaController);
+//        videoView.setVideoURI(videoURI);
+//        videoView.requestFocus();
+//
+//        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+//            @Override
+//            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+//                Toast.makeText (getBaseContext(), "Error", Toast.LENGTH_LONG).show ();
+//                return true;
+//            }
+//        });
+//
+//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mediaPlayer) {
+//                videoView.start ();
+//            }
+//        });
+//
+//        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            @Override
+//            public void onCompletion(MediaPlayer mediaPlayer) {
+//                mediaPlayer.reset ();
+//            }
+//        });
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
